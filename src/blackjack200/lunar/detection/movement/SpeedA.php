14a3c0dd5947a6976e1a2277dd93dbd125984877
Blackjack200 <lx826444126@gmail.com>
2021-04-05 07:35:10 +0800

    fix(SpeedA): correct prediction value
    thank you very much @ethaniccc

--- src/blackjack200/lunar/detection/movement/SpeedA.php
+++ src/blackjack200/lunar/detection/movement/SpeedA.php
@@ -29,7 +29,7 @@ class SpeedA extends DetectionBase {
 			$curt = hypot($curtMD->x, $curtMD->z);
 
 			//reference: https://github.com/GladUrBad/Medusa/blob/7be8d34ae0470f0655b59e213d7619b98a3f43ff/Impl/src/main/java/com/gladurbad/medusa/check/impl/movement/speed/SpeedA.java#L25
-			$predicted = $last * 0.91 + ($user->getPlayer()->isSprinting() ? 0.0263 : 0.02);
+			$predicted = $last * 0.91 + ($user->getPlayer()->isSprinting() ? 0.026 : 0.02);
 			$diff = $curt - $predicted;
 			if ($predicted > 0.075 &&
 				$diff > $this->maxDiff &&
