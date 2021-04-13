c7e2592c73e0105d9ff3ede9c7acf07f5f1d7f9c
Blackjack200 <lx826444126@gmail.com>
2021-04-13 22:34:11 +0800

    feat: GroundSpoofA

--- src/blackjack200/lunar/StandardDetectionRegistry.php
+++ src/blackjack200/lunar/StandardDetectionRegistry.php
@@ -13,6 +13,7 @@ use blackjack200\lunar\detection\DetectionBase;
 use blackjack200\lunar\detection\movement\fly\FlyA;
 use blackjack200\lunar\detection\movement\fly\FlyB;
 use blackjack200\lunar\detection\movement\fly\FlyE;
+use blackjack200\lunar\detection\movement\GroundSpoofA;
 use blackjack200\lunar\detection\movement\speed\SpeedA;
 use blackjack200\lunar\detection\movement\speed\SpeedC;
 use blackjack200\lunar\detection\packet\BadPacketA;
@@ -40,6 +41,7 @@ final class StandardDetectionRegistry {
 			'FlyA' => FlyA::class,
 			'FlyB' => FlyB::class,
 			'FlyE' => FlyE::class,
+			'GroundSpoofA' => GroundSpoofA::class,
 		];
 
 		foreach (self::$detections as $name => $class) {
